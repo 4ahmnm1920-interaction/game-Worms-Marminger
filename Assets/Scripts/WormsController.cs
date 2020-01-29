7d3aa7818666fec98b00a17f041e9da709b431d7
maximaarminger <maxima.arminger@htl-salzburg.ac.at>
2020-01-29 11:30:30 +0100

    Game Worm

---
+++ Assets/Scripts/WormsController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormsController : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float jumpforce;
+    public float moveForward;
+    public float moveBackward;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+        
+            Vector3 force = new Vector3(0,jumpforce,0);
+
+            rb.AddForce(force);
+            Debug.Log("Taste ist gedrückt!");
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            Vector3 For = new Vector3(moveForward, 0, 0);
+
+            rb.AddForce(For);
+            Debug.Log("Taste ist gedrückt!");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            Vector3 Back = new Vector3(0, 0, moveBackward);
+
+            rb.AddForce(Back);
+            Debug.Log("Taste ist gedrückt!");
+        }
+
+    }
+}
